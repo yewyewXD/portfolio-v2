af71ecec123c383aaa507cf7812229363a7a7d87
Yew Kang Wei <yewyew6933@gmail.com>
2021-02-15 11:34:59 +0800

    add testimonial

--- src/components/section/Testimonial.jsx
+++ src/components/section/Testimonial.jsx
@@ -7,15 +7,28 @@ export default function Testimonial() {
   const testimonials = [
     {
       text:
-        "It was an absolute pleasure to be working with KangWei as he did what he had written in the proposal, which is make the front end pixel perfect and even implement some changes. Also, he was always very prompt and kind in his communications.",
+        "Yew was great to work with, was very quick and understood exactly what we needed. We will be using his services again when we need more work in this area.",
       source:
-        "https://www.linkedin.com/posts/yewyewxd_see-how-i-can-get-your-project-done-on-upwork-activity-6709339082209792000-RWFU",
+        "https://www.linkedin.com/posts/yewyewxd_see-how-i-can-get-your-project-done-on-upwork-activity-6766884031473815552-DN58",
     },
     {
       text:
         "Great developer! He is extremely passionate and knowledgeable about the work he does.",
       source:
         "https://www.linkedin.com/posts/yewyewxd_see-how-i-can-get-your-project-done-on-upwork-activity-6710509017812357120-i5Lo",
+    },
+    {
+      text:
+        "It was an absolute pleasure to be working with KangWei as he did what he had written in the proposal, which is make the front end pixel perfect and even implement some changes. Also, he was always very prompt and kind in his communications.",
+      source:
+        "https://www.linkedin.com/posts/yewyewxd_see-how-i-can-get-your-project-done-on-upwork-activity-6709339082209792000-RWFU",
+    },
+
+    {
+      text:
+        "Great Work!!! Very Fast!! Professional and Talented Work. Provided Exactly what I wanted 10 Stars!!",
+      source:
+        "https://www.linkedin.com/posts/yewyewxd_see-how-i-can-get-your-project-done-on-upwork-activity-6705346768080646144-_FFL",
     },
   ];
   const sliderSettings = {
