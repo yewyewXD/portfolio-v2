ecbd222098d8e2259f5de4c2562343b9adebcff9
unknown <yewyew6933@gmail.com>
2021-03-14 09:31:03 +0800

    refactor my skills section

--- src/components/section/Skill.jsx
+++ src/components/section/Skill.jsx
@@ -1,114 +1,95 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { BiCodeAlt, BiBookHeart } from "react-icons/bi";
 import { CgDatabase } from "react-icons/cg";
 import { VscTools } from "react-icons/vsc";
 import { GrUserSettings } from "react-icons/gr";
 
 export default function Skill() {
-  const frontendSkillItems = [
-    "HTML5",
-    "CSS",
-    "Sass",
-    "Bootstrap",
-    "JavaScript",
-    "ReactJS",
-    "NextJS",
-    "GatsbyJS",
-  ];
-  const backendSkillItems = ["NodeJS", "ExpressJS", "MongoDB"];
-  const devToolSkillItems = [
-    "Git",
-    "Postman",
-    "Jira",
-    "Stripe",
-    "Contentful",
-    "Netlify",
-  ];
-  const personalitySkillItems = [
-    "Super-Hardworking",
-    "Patient",
-    "Attentive",
-    "Empathetic",
-    "Optimistic",
-  ];
-  const interestsSkillItems = [
-    "Blockchain",
-    "Digital Marketing",
-    "Mobile App Dev",
-  ];
+  const skillSections = useMemo(() => {
+    return [
+      {
+        id: "skillSection-1",
+        icon: <BiCodeAlt className="mr-2" />,
+        heading: "Frontend",
+        skillItems: [
+          "HTML5",
+          "CSS",
+          "Sass",
+          "Bootstrap",
+          "JavaScript",
+          "ReactJS",
+          "NextJS",
+          "GatsbyJS",
+        ],
+      },
+      {
+        id: "skillSection-2",
+        icon: <CgDatabase className="mr-2" />,
+        heading: "Backend",
+        skillItems: ["NodeJS", "ExpressJS", "MongoDB"],
+      },
+      {
+        id: "skillSection-3",
+        icon: <VscTools className="mr-2" />,
+        heading: "Dev Tools",
+        skillItems: [
+          "Git",
+          "Postman",
+          "Jira",
+          "Stripe",
+          "Contentful",
+          "Netlify",
+        ],
+      },
+      {
+        id: "skillSection-4",
+        icon: <GrUserSettings className="mr-2" />,
+        heading: "Personality",
+        skillItems: [
+          "Super-Hardworking",
+          "Patient",
+          "Attentive",
+          "Empathetic",
+          "Optimistic",
+        ],
+      },
+
+      {
+        id: "skillSection-5",
+        icon: <BiBookHeart className="mr-2" />,
+        heading: "Interests",
+        skillItems: [
+          "Blockchain",
+          "Digital Marketing",
+          "Mobile Dev",
+          "Fulllstack Dev",
+        ],
+      },
+    ];
+  }, []);
 
   return (
     <section id="skill" className="py-5 bg-light text-sm-left text-center">
       <div className="container my-sm-5 my-3">
         <h1 className="mb-5 ">My skills</h1>
 
-        <div className="skill mb-4" data-aos="fade-up">
-          <h2 className="all-center justify-content-sm-start">
-            <BiCodeAlt className="mr-2" /> Frontend
-            <span className="ml-2 d-sm-inline d-none">Development</span>
-          </h2>
-          <div className="skill-items justify-content-sm-start justify-content-center d-flex flex-wrap">
-            {frontendSkillItems.map((skillItem) => (
-              <span className="skill-item mr-sm-3 mx-sm-0 mx-2" key={skillItem}>
-                {skillItem}
-              </span>
-            ))}
+        {skillSections.map((skillSection) => (
+          <div className="skill mb-4" data-aos="fade-up" key={skillSection.id}>
+            <h2 className="all-center justify-content-sm-start">
+              {skillSection.icon} {skillSection.heading}
+            </h2>
+            <div className="skill-items justify-content-sm-start justify-content-center d-flex flex-wrap">
+              {skillSection.skillItems.map((skillItem) => (
+                <span
+                  className="skill-item mr-sm-3 mx-sm-0 mx-2"
+                  key={skillItem}
+                >
+                  {skillItem}
+                </span>
+              ))}
+            </div>
           </div>
-        </div>
-
-        <div className="skill mb-4" data-aos="fade-up">
-          <h2 className="all-center justify-content-sm-start">
-            <CgDatabase className="mr-2" /> Backend
-            <span className="ml-2 d-sm-inline d-none">Development</span>
-          </h2>
-          <div className="skill-items justify-content-sm-start justify-content-center d-flex flex-wrap">
-            {backendSkillItems.map((skillItem) => (
-              <span className="skill-item mr-sm-3 mx-sm-0 mx-2" key={skillItem}>
-                {skillItem}
-              </span>
-            ))}
-          </div>
-        </div>
-
-        <div className="skill mb-4" data-aos="fade-up">
-          <h2 className="all-center justify-content-sm-start">
-            <VscTools className="mr-2" /> Dev
-            <span className="d-sm-inline d-none mr-2">eloper</span> Tools
-          </h2>
-          <div className="skill-items justify-content-sm-start justify-content-center d-flex flex-wrap text-center">
-            {devToolSkillItems.map((skillItem) => (
-              <span className="skill-item mr-sm-3 mx-sm-0 mx-2" key={skillItem}>
-                {skillItem}
-              </span>
-            ))}
-          </div>
-        </div>
-
-        <div className="skill mb-4" data-aos="fade-up">
-          <h2 className="all-center justify-content-sm-start">
-            <GrUserSettings className="mr-2" /> Personality
-          </h2>
-          <div className="skill-items justify-content-sm-start justify-content-center d-flex flex-wrap text-center">
-            {personalitySkillItems.map((skillItem) => (
-              <span className="skill-item mr-sm-3 mx-sm-0 mx-2" key={skillItem}>
-                {skillItem}
-              </span>
-            ))}
-          </div>
-        </div>
-
-        <div className="skill mb-4" data-aos="fade-up">
-          <h2 className="all-center justify-content-sm-start">
-            <BiBookHeart className="mr-2" /> Other Interests
-          </h2>
-          <div className="skill-items justify-content-sm-start justify-content-center d-flex flex-wrap text-center">
-            {interestsSkillItems.map((skillItem) => (
-              <span className="skill-item mr-sm-3 mx-sm-0 mx-2" key={skillItem}>
-                {skillItem}
-              </span>
-            ))}
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
